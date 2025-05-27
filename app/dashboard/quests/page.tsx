4cb5eddcaaeec85e1626b0f9ee4a8c25eac259ef
nazsats <nazsats@gmail.com>
2025-05-27 13:37:59 +0530

    quest updated

--- app/dashboard/quests/page.tsx
+++ app/dashboard/quests/page.tsx
@@ -14,7 +14,7 @@ const INITIAL_QUESTS = [
   { id: 'connect_twitter', title: 'Connect Twitter', description: 'Link your Twitter account', meowMiles: 30, completed: false, icon: '/quest/link.png' },
   { id: 'connect_discord', title: 'Connect Discord', description: 'Link your Discord account', meowMiles: 30, completed: false, icon: '/quest/discord.png' },
   { id: 'follow_twitter', title: 'Follow Twitter', description: 'Follow @catcentsio on Twitter', meowMiles: 30, completed: false, icon: '/quest/x.png', taskUrl: 'https://twitter.com/catcentsio' },
-  { id: 'follow_anselmeow', title: 'Follow Anselmeow', description: 'Follow @Anselmeow_ on Twitter', meowMiles: 30, completed: false, icon: '/quest/x.png', taskUrl: 'https://x.com/Anselmeow_' },
+  // { id: 'follow_anselmeow', title: 'Follow Anselmeow', description: 'Follow @Anselmeow_ on Twitter', meowMiles: 30, completed: false, icon: '/quest/x.png', taskUrl: 'https://x.com/Anselmeow_' },
   { 
     id: 'share_post', 
     title: 'Share a Post', 
